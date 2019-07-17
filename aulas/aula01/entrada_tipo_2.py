2aa03d97f58b2d740eca1cd45d2614531b4362ef
C1296467 Rodrigo Guimaraes Araujo <c1296467@bb.com.br>
2019-07-17 11:24:20 -0300

    entradas e exemplo 1

---
+++ aulas/aula01/entrada_tipo_2.py
@@ -0,0 +1,4 @@
+qtd = int(input())
+for _ in range(qtd):
+    x, y = map(int, input().split())
+    print(x+y)
---
+++ aulas/aula01/uva_100.py
@@ -0,0 +1,21 @@
+# https://uva.onlinejudge.org/index.php?Itemid=8&option=com_onlinejudge&page=show_problem&problem=36
+
+def circle(n):
+    qtd = 0
+    while True:
+        qtd += 1
+        if n == 1:
+            break
+        n = (3*n + 1) if n%2 != 0 else n//2
+    return qtd
+
+while True:
+    try:
+        x, y = map(int, input().split())
+        ans, a, b = 0, min([x, y]), max([x, y])
+        while a <= b:
+            ans = max(ans, circle(a))
+            a += 1
+        print(x, y, ans)
+    except EOFError:
+        break
